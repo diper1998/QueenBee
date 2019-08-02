db3039e73553fe7667c519abdc29c5509afc778f
Dima Perov <33723506+diper1998@users.noreply.github.com>
2019-08-03 00:39:42 +0300

    add Platforms Devices Info

--- include/QueenBee.hpp
+++ include/QueenBee.hpp
@@ -1,16 +1,45 @@
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
-
 #define __CL_ENABLE_EXCEPTIONS
-
 #if defined(__APPLE__) || defined(__MACOSX)
 #include <OpenCL/cl.hpp>
 #else
 #include <CL/cl.hpp>
 #endif
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
 #include <vector>
-using namespace std;
 
-void Hello();
+using namespace std;
+using namespace cl;
+
+class Hive {  // Device
+ protected:
+  Device device;
+  string name;
+  string type;
+
+ public:
+  Hive();
+
+  friend class Keeper;
+};
+
+class Garden {  // Platform
+ protected:
+  Platform platform;
+  vector<Hive> hives;
+  string name;
+
+ public:
+  Garden();
+
+  friend class Keeper;
+};
+
+class Keeper {
+ protected:
+  vector<Garden> gardens;
+
+ public:
+  Keeper();
+  void Info();
+};
--- src/QueenBee.cpp
+++ src/QueenBee.cpp
@@ -1,5 +1,53 @@
 #include "QueenBee.hpp"
 
-void Hello () {
-	cout << "Hello, World!";
+Keeper::Keeper() {
+  vector<Platform> platforms;
+  Platform::get(&platforms);
+
+  Hive hive;
+  Platform platform;
+  vector<Device> devices;
+
+  Garden garden;
+
+  for (auto p : platforms) {
+    garden.platform = p;
+    garden.name = p.getInfo<CL_PLATFORM_NAME>();
+
+    p.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+    for (auto d : devices) {
+      hive.device = d;
+      hive.name = d.getInfo<CL_DEVICE_NAME>();
+
+      switch (d.getInfo<CL_DEVICE_TYPE>()) {
+        case CL_DEVICE_TYPE_CPU:
+          hive.type = "CPU";
+          break;
+        case CL_DEVICE_TYPE_GPU:
+          hive.type = "GPU";
+          break;
+        case CL_DEVICE_TYPE_ACCELERATOR:
+          hive.type = "ACCELERATOR";
+          break;
+      }
+
+      garden.hives.push_back(hive);
+    }
+
+    gardens.push_back(garden);
+  }
 }
+
+void Keeper::Info() {
+  for (auto g : gardens) {
+    cout << g.name << endl;
+    for (auto h : g.hives) {
+      cout << h.type << ": " << h.name << endl;
+    }
+  }
+}
+
+Garden::Garden() {}
+
+Hive::Hive() {}
